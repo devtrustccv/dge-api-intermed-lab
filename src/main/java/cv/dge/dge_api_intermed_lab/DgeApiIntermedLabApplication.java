1c08a9769e74e9bfef8da9b9e44ce2e768b21326
Adnilson Barbosa <130472830+AdnilsonBarbosa@users.noreply.github.com>
2026-04-09 11:56:59 -0100

    v1

---
+++ src/main/java/cv/dge/dge_api_intermed_lab/DgeApiIntermedLabApplication.java
@@ -0,0 +1,12 @@
+package cv.dge.dge_api_intermed_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgeApiIntermedLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgeApiIntermedLabApplication.class, args);
+    }
+}
